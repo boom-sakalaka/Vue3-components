2878c6f1b42bc3f65b5e7a655b10d71fc0516f7b
Gu <936285702@qq.com>
2021-10-04 14:31:45 +0800

    展示demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,58 @@
  * @Author: GZH
  * @Date: 2021-10-02 15:11:35
  * @LastEditors: GZH
- * @LastEditTime: 2021-10-02 18:25:43
+ * @LastEditTime: 2021-10-04 14:31:04
  * @FilePath: \vue3-json-schema-form\src\App.tsx
  * @Description:
  */
-import { withModifiers, defineComponent, ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, ref, Ref } from 'vue'
+
+import { createUseStyles } from 'vue-jss'
+
+import MonacoEditor from './components/MonacoEditor'
+function toJson(data: unknown) {
+  return JSON.stringify(data, null, 2)
+}
+
+const schema = {
+  type: 'string',
+}
+
+const useSytles = createUseStyles({
+  editor: {
+    minHeight: 400,
+  },
+})
 
 const App = defineComponent({
   setup() {
-    const count = ref(0)
-
-    const inc = () => {
-      count.value++
+    const shemaRef: Ref<unknown> = ref(schema)
+    const handelCodeChange = (code: string) => {
+      let shema: unknown
+      try {
+        shema = JSON.parse(code)
+      } catch (err) {
+        console.warn(err)
+      }
+      shemaRef.value = shema
     }
+    const classesRef = useSytles()
+    return () => {
+      const calsses = classesRef.value
 
-    return () => (
-      <div onClick={withModifiers(inc, ['self'])}>
-        {count.value}
-        <HelloWorld />
-      </div>
-    )
+      const code = toJson(shemaRef.value)
+
+      return (
+        <div>
+          <MonacoEditor
+            code={code}
+            onChange={handelCodeChange}
+            title="Schema"
+            class={calsses.editor}
+          />
+        </div>
+      )
+    }
   },
 })
 
